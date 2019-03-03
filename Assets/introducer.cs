dd34a24d8f70e3933e8a88a8dd4f5a15500a30f8
Ravin Kumar <mr.ravin_kumar@hotmail.com>
2019-03-03 17:04:05 +0530

    Add files via upload

---
+++ Assets/introducer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class introducer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello rav");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/playermovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    Rigidbody rbody;
+    public GameObject player;       //Public variable to store a reference to the player game object
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        process_keystroke();
+    }
+    void process_keystroke()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rbody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rbody.AddRelativeForce(Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rbody.AddRelativeForce(Vector3.back);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.down);
+        }
+    }
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = player.transform.position + offset;
+    }
+}
